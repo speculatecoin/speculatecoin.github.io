02de5217395e07167a9639233404cdc0b048a29f
speculatecoin <30680810+speculatecoin@users.noreply.github.com>
2017-08-08 00:54:27 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -5,18 +5,18 @@ $(function() {
             var version = web3.version.api;
             var count;
 
-            $.getJSON('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x2c39f92294f436c956f12dcd801a3181e77f8851&address=0x2c39f92294f436c956f12dcd801a3181e77f8851&tag=latest&apikey=VZEIQ7N8AJDXPKNWP21HB1BG5WNXEU535R', function (jsonData) {
+            $.getJSON('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x2a749bAc8B1D129038A739fa7cf52d476E9f7845&address=0x2a749bAc8B1D129038A739fa7cf52d476E9f7845&tag=latest&apikey=VZEIQ7N8AJDXPKNWP21HB1BG5WNXEU535R', function (jsonData) {
                 count = jsonData.result;
                 $('#weresold').html((2100000000000000-count)/100000000);
                 
             }.bind(this));
 
-            $.getJSON('https://api.etherscan.io/api?module=contract&action=getabi&address=0x2c39F92294F436c956F12dCd801a3181e77F8851', function (data) {
+            $.getJSON('https://api.etherscan.io/api?module=contract&action=getabi&address=0x2a749bAc8B1D129038A739fa7cf52d476E9f7845', function (data) {
                 var contractABI = "";
                 contractABI = JSON.parse(data.result);
                 if (contractABI != '') {
                     var MyContract = web3.eth.contract(contractABI);
-                    var myContractInstance = MyContract.at('0x2c39F92294F436c956F12dCd801a3181e77F8851');
+                    var myContractInstance = MyContract.at('0x2a749bAc8B1D129038A739fa7cf52d476E9f7845');
                     $('#currentprice').html(((myContractInstance.transactions()*100+100)/10000000000).toFixed(8));
                 } 
                 else {
